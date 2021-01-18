14a49f8d5b2430d1ec6d86b100efd2d0adc15c0b
Céline PELLETIER <celine_pelletier@hotmail.fr>
2021-01-18 21:00:02 +0100

    ajout html

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from website import views
 
 urlpatterns = [
-    path('', views.list_product, name="list_product"),
+    path('', views.HomeView.as_view(), name="home"),
+    path('mesproduits', views.list_product, name="list_product")
 ]
--- website/views.py
+++ website/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.views import View
 
 from website.models import Product
+
+
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'home.html')
+
+
+
 
 
 def list_product(request):
