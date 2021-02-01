8f7ce75a7696320f320330d0a7724ef49e238aab
Céline PELLETIER <celine_pelletier@hotmail.fr>
2021-02-01 18:04:54 +0100

    ajout de logout que si je suis connecté.

--- website/urls.py
+++ website/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('signup/', website_views.signup, name='signup'),
     path('login/', views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
