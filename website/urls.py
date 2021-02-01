511ac556823b879d392200001fc50730afc3bc48
Céline PELLETIER <celine_pelletier@hotmail.fr>
2021-02-01 17:54:27 +0100

    ajout page login.html et signup.html dans un lien sur la page login.html

--- website/urls.py
+++ website/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-
-from website import views
+from django.contrib.auth import views
+from website import views as website_views
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name="home"),
-    path('mesaliments/', views.MyProductView.as_view(), name="my_product"),
-    path('detail/<slug:slug>/', views.ProductView.as_view(), name="detail"),
+    path('', website_views.HomeView.as_view(), name="home"),
+    path('mesaliments/', website_views.MyProductView.as_view(), name="my_product"),
+    path('detail/<slug:slug>/', website_views.ProductView.as_view(), name="detail"),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('signup/', views.signup, name='signup'),
+    path('signup/', website_views.signup, name='signup'),
+    path('login/', views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
