69cfa6ef9c0129d00715954bda28bd3b1622964b
Céline PELLETIER <celine_pelletier@hotmail.fr>
2021-02-26 20:37:23 +0100

    2e commit pour faire passer le boostrap.css

--- PurBeurreSite/settings.py
+++ PurBeurreSite/settings.py
@@ -127,9 +127,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'website/static/website')]
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 AUTH_USER_MODEL = 'website.User'
 
