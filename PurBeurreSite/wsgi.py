ed93f49082c8acb3685b9eb4e223150b64819f7b
Céline PELLETIER <celine_pelletier@hotmail.fr>
2021-02-26 21:29:34 +0100

    5e toujours le procfile

--- PurBeurreSite/wsgi.py
+++ PurBeurreSite/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PurBeurreSite.settings.staging")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PurBeurreSite.settings")
 
 application = get_wsgi_application()
