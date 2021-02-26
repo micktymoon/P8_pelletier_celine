bff83cac0b9491ce851909ba6eb72eb8934ad040
Céline PELLETIER <celine_pelletier@hotmail.fr>
2021-02-26 19:35:33 +0100

    premier commit pour heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn PurBeurreSite.wsgi --log-file -
--- PurBeurreSite/settings.py
+++ PurBeurreSite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'neb(@$#u00qt8rc1-(*383lpd96v7p_%ed2v(x_ndk8&oo$6vo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False if os.environ.get("ENV", "development") == "production" else True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapps.com', 'localhost', '127.0.0.1']
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = 'home'
@@ -129,9 +129,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 AUTH_USER_MODEL = 'website.User'
 
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
+
+django_heroku.settings(locals())
