2e9816e81ad73a171f03b43437046917f05e89cc
Céline PELLETIER <celine_pelletier@hotmail.fr>
2021-05-10 18:26:44 +0200

    revert commit pour revenir au settings normal et tout les tests sont ok

--- PurBeurreSite/urls.py
+++ PurBeurreSite/urls.py
@@ -22,5 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
-    path('sentry-debug/', 1/0),
 ]
