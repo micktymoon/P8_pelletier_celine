2b92368d7cee83356f9f26bc4a0b5cb1c9b96399
Céline PELLETIER <celine_pelletier@hotmail.fr>
2021-05-15 16:25:05 +0200

    modification du repo. re-root le repo

--- website/admin.py
+++
@@ -1,9 +0,0 @@
-
-
-from django.contrib import admin
-from .models import Product, User, Store, Category
-
-admin.site.register(Product)
-admin.site.register(User)
-admin.site.register(Store)
-admin.site.register(Category)
