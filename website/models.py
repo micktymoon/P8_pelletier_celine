b059c185a6c301a70ed77c1082a083489a9592b6
Céline PELLETIER <celine_pelletier@hotmail.fr>
2021-05-15 17:45:34 +0200

    modif models pour éviter le nutriscore null

--- website/models.py
+++ website/models.py
@@ -16,7 +16,7 @@ class Product(models.Model):
     """Store a product in the database"""
     name = models.CharField(max_length=255)
     brand = models.CharField(max_length=255, null=True)
-    nutri_score = models.CharField(max_length=2)
+    nutri_score = models.CharField(max_length=2, null=True)
     url_off = models.TextField(null=True)
     url_image = models.TextField(null=True)
     nutriments_100g = models.TextField(null=True)
